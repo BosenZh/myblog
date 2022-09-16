51c7d2bb9caaf07052a9e77d70bce81140051a31
Bosen Zhang <bosenzhang2002@gmail.com>
2022-09-16 16:31:30 -0400

    bug fix

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Style from "./Home.module.scss";
-import me from "../../img/self.png";
-import classNames from "classnames";
 import EmojiBullet from "./EmojiBullet";
 import SocialIcon from "./SocialIcon";
 import { Box } from "@mui/material";
